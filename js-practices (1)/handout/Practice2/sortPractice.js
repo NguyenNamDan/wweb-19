57c4000b9750007d90184b2c4b348a002abff3d0
Dan Nguyen <nguyennamdanbk2610@gmail.com>
2019-02-17 09:37:38 +0700

    commit

--- js-practices (1)/handout/Practice2/sortPractice.js
+++ js-practices (1)/handout/Practice2/sortPractice.js
@@ -1,7 +1,18 @@
 'use strict'
 
 function sort(input) {
-  return input.sort((a,b) => a-b); // Remove this line and change to your own algorithm
+  // return input.sort((a,b) => a-b); // Remove this line and change to your own algorithm
+  let bigNumber;
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] > input[j]) {
+        bigNumber = input[i];
+        input[i] = input[j];
+        input[j] = bigNumber;
+      }
+    }
+  }
+  return input;
 }
 
 module.exports = sort
--- js-practices (1)/handout/Practice3/generatePractice.js
+++ js-practices (1)/handout/Practice3/generatePractice.js
@@ -5,9 +5,10 @@ function generate(testLengthArray){
     .map(item => ({
       input: Array.from({length: item}).map(item => []),
       target: 0,
-      output: -1
+      output: -1  
     })
   ); // Remove this line and change to your own algorithm
-}
+} 
+// em ko làm đc bài này ạ huhuh :v 
 
 module.exports = generate
